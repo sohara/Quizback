f69e9940d2ccf0898aeb25d3323b06af50dcbdd1
Sean O'Hara <sohara@sohara.com>
2011-11-15 16:40:07 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizback::Application.config.secret_token = 'cc44536c13f9977b4e41915af0cb9f3d394cac5a3048165c15fecc0a11f214c74049176f9dd8084724856f9513e4c9642be81d0a4de22a87d9903612522c5520'
